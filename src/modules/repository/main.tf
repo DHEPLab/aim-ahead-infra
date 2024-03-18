405f2539c374b4af1db51d2c91858a8eedf596d0
haoran.li <haoran.li1@thoughtworks.com>
2024-03-18 15:29:42 +0800

    [UNCAIM-92][haoran.li]feat: remove unused repository

--- src/modules/repository/main.tf
+++ src/modules/repository/main.tf
@@ -7,15 +7,6 @@ terraform {
   }
 }
 
-resource "aws_ecr_repository" "ecr_repo" {
-  name = "${var.project_name}-repository"
-
-  image_tag_mutability = "IMMUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
 resource "aws_ecr_repository" "aim_ahead_api_repo" {
   name = local.api_image_name
   #trivy:ignore:avd-aws-0031
