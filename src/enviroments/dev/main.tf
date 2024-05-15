7251e17bb92e263ea8c8748de843b539bfe2ab81
xinyang.li <xinyang.li@thoughtworks.com>
2024-05-15 11:19:26 +0800

    [UNCAIM-126][Xinyang]feat: remove unused task role

--- src/enviroments/dev/main.tf
+++ src/enviroments/dev/main.tf
@@ -26,31 +26,6 @@ provider "aws" {
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "${local.project_name}-ecs-task-executor-${local.environment}"
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
-}
-EOF
-
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 module "tf-state" {
   source = "../../modules/state"
 
